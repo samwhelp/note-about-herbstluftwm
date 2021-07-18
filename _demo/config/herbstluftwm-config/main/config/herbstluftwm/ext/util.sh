8591c1735ec246e7e09544dbde2c6be18323f337
samwhelp <samwhelp@users.noreply.github.com>
2021-07-18 18:09:55 +0800

    util process

--- _demo/config/herbstluftwm-config/main/config/herbstluftwm/ext/util.sh
+++ _demo/config/herbstluftwm-config/main/config/herbstluftwm/ext/util.sh
@@ -1,7 +1,7 @@
 
 
 ################################################################################
-### Head: Util_Debug
+### Head: Util / Debug
 ##
 
 util_debug_echo () {
@@ -11,12 +11,12 @@ util_debug_echo () {
 }
 
 ##
-### Head: Util_Debug
+### Head: Util / Debug
 ################################################################################
 
 
 ################################################################################
-### Head: Util_Command
+### Head: Util  / Command
 ##
 
 is_function_exist () {
@@ -44,5 +44,48 @@ is_command_exist () {
 }
 
 ##
-### Tail: Util_Command
+### Tail: Util / Command
+################################################################################
+
+
+################################################################################
+### Head: Util / Process
+##
+
+util_stop_all ()  {
+
+	if is_command_exist 'pkill'; then
+		util_stop_all_by_pkill "$1"
+		return 0
+	fi
+
+	if is_command_exist 'killall'; then
+		util_stop_all_by_killall "$1"
+		return 0
+	fi
+
+	return 0
+}
+
+util_stop_all_by_pkill () {
+
+	if pkill "$1"; then
+		return 0
+	fi
+
+	return 0
+}
+
+util_stop_all_by_killall () {
+
+	if killall -q -9 "$1"; then
+		return 0
+	fi
+
+	return 0
+
+}
+
+##
+### Tail: Util / Process
 ################################################################################
