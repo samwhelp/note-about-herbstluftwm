d226c2b8d8694caddf8000354ab1729678d06911
samwhelp <samwhelp@users.noreply.github.com>
2021-07-21 15:24:57 +0800

    completion

--- _demo/config/herbstluftwm-config/main/config/herbstluftwm/bin/maintain-install-completion-manjaro.sh
+++ _demo/config/herbstluftwm-config/main/config/herbstluftwm/bin/maintain-install-completion-manjaro.sh
@@ -33,6 +33,15 @@ mod_install_completion_bash () {
 	echo "sudo install -Dm644 $THE_SHARE_DIR_PATH/completion/bash/hlwm-profile-ctrl /usr/local/share/bash-completion/completions/hlwm-profile-ctrl"
 	sudo install -Dm644 "$THE_SHARE_DIR_PATH/completion/bash/hlwm-profile-ctrl" "/usr/local/share/bash-completion/completions/hlwm-profile-ctrl"
 
+	echo "sudo install -Dm644 $THE_SHARE_DIR_PATH/completion/bash/hlwm-frame-ctrl /usr/local/share/bash-completion/completions/hlwm-frame-ctrl"
+	sudo install -Dm644 "$THE_SHARE_DIR_PATH/completion/bash/hlwm-frame-ctrl" "/usr/local/share/bash-completion/completions/hlwm-frame-ctrl"
+
+	echo "sudo install -Dm644 $THE_SHARE_DIR_PATH/completion/bash/hlwm-window-ctrl /usr/local/share/bash-completion/completions/hlwm-window-ctrl"
+	sudo install -Dm644 "$THE_SHARE_DIR_PATH/completion/bash/hlwm-window-ctrl" "/usr/local/share/bash-completion/completions/hlwm-window-ctrl"
+
+	echo "sudo install -Dm644 $THE_SHARE_DIR_PATH/completion/bash/hlwm-wallpaper-ctrl /usr/local/share/bash-completion/completions/hlwm-wallpaper-ctrl"
+	sudo install -Dm644 "$THE_SHARE_DIR_PATH/completion/bash/hlwm-wallpaper-ctrl" "/usr/local/share/bash-completion/completions/hlwm-wallpaper-ctrl"
+
 }
 
 
