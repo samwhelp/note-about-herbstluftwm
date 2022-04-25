f1bda104333fbfae51b0ed8b9ca385a56ea10b35
samwhelp <samwhelp@users.noreply.github.com>
2022-04-25 19:26:38 +0800

    fix

--- _demo/config/herbstluftwm-config/main/config/herbstluftwm/sys/profile/main/rule.sh
+++ _demo/config/herbstluftwm-config/main/config/herbstluftwm/sys/profile/main/rule.sh
@@ -8,12 +8,13 @@ skel_rule () {
 
 	# rules
 	hc unrule -F
-	#hc rule class=XTerm tag=1 # move all xterms to tag 1
-	#hc rule class=Sakura tag=1 # move all Sakura to tag 1
+
+	hc rule class=pcmanfm-qt tag=1 # move all Pcmanfm-qt to tag 1
+	hc rule class=Thunar tag=1 # move all Thunar to tag 1
 	hc rule class=Atom tag=2 # move all Atom to tag 2
 	hc rule class=firefox tag=3 # move all Firefox to tag 3
-	hc rule class=pcmanfm-qt tag=4 # move all Pcmanfm-qt to tag 4
-	hc rule class=Thunar tag=4 # move all Thunar to tag 4
+	#hc rule class=XTerm tag=4 # move all xterms to tag 4
+	#hc rule class=Sakura tag=4 # move all Sakura to tag 4
 
 	hc rule focus=on # normally focus new clients
 	hc rule floatplacement=smart
